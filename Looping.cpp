4d9a3b21a40277327b44ba2f297c28e068598961
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-03-30 15:04:04 +0700

    Menambahkan perulangan FOR

--- Looping.cpp
+++ Looping.cpp
@@ -5,4 +5,12 @@ using namespace std;
 int main(){
     int x;
 
+ //Perulangan dengan FOR
+    //mencetak tulisan seabnyak 5 kali
+    cout<<"PERULANGAN FOR"<<endl;
+    for(int i=0;i<=4;i++){
+        cout<<"Teknologi Informasi UMY"<<endl;
+    }
+    cout<<endl;
+
     
