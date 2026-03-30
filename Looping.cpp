4651caf24bd6a04fae20876f89a8dde8d367c77c
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-03-30 15:07:07 +0700

    Implementasi Perulangan While

--- Looping.cpp
+++ Looping.cpp
@@ -16,6 +16,16 @@ int main(){
     //untuk mendapatkan urutan angka acak yg berbeda
     srand(time(0));
 
-    /
+    //Perulangan dengan WHILE
+    cout<<"PERULANGAN WHILE"<<endl;
+    x = 1 + rand()%10;
 
-   
+    while(x<=5){
+        cout<<"Bilangan acak = "<<x<<endl;
+        x = rand()%10;
+    }
+
+    cout<<"Bilangan acak while yang terakhir = "<<x<<endl;
+    cout<<endl;
+
+    s
